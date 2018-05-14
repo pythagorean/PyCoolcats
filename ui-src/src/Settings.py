5009587c591596b9bc10f5ac299f4f0904eff234
Michael Goldman <mike.goldman@allstreets.org>
2018-05-14 17:31:10 -0400

    Settings converted to Python

--- ui-src/src/Settings.py
+++ ui-src/src/Settings.py
@@ -1,51 +1,57 @@
 __pragma__('js', '{}', '''
-import React, { Component } from 'react'
-
-class Settings extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      newHandleText: ''
-    }
-  }
-  updateHandleText = (e) => {
-    this.setState({
-      newHandleText: e.target.value
-    })
-  }
-  onHandleSubmit = (e) => {
-    e.preventDefault()
-    this.setState({
-      newHandleText: ''
-    })
-    if (!this.state.newHandleText) return
-    this.props.newHandle(this.state.newHandleText)
-    this.setState({
-      newHandleText: ''
-    })
-  }
-  render () {
-    return (
-      <div className='panel panel-default'>
-        <div className='panel-body'>
-          <h3 id='setHandleModalLabel'>Set your handle</h3>
-          <p style={{display: (this.props.handleTaken === true) ? 'inline' : 'none'}}>Handle already taken try another one</p>
-          <form id='handleForm' onSubmit={this.onHandleSubmit} className='form-group'>
-            <div className='col-xs-8'>
-              <div className="form-group input-icon">
-                <i>@</i>
-                <input value={this.state.newHandleText} onChange={this.updateHandleText} type='text' className='form-control' id='myHandle' placeholder='handle' />
-              </div>
-            </div>
-            <div className='col-xs-2'>
-              <button id='setHandleButton' type='submit' className='btn btn-primary'>Set Handle</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default Settings
+import { createElement as e } from 'react'
+import createReactClass from 'create-react-class'
+const { div, h3, p, i, form, input, button } = require('hyperscript-helpers')(e)
 ''')
+
+def settingsOnHandleSubmit(self, handle):
+    handle.preventDefault()
+    self.setState({ 'newHandleText': '' })
+    if not self.state['newHandleText']: return
+    self.props.newHandle(self.state['newHandleText'])
+    self.setState({ 'newHandleText': '' })
+
+Settings = createReactClass({
+    'getInitialState': lambda: { 'newHandleText': '' },
+
+    'updateHandleText': lambda handle: this.setState(
+        { 'newHandleText': handle.target.value }),
+
+    'onHandleSubmit': lambda handle: settingsOnHandleSubmit(this, handle),
+
+    'render': lambda: div({ 'className': 'panel panel-default' },
+        div({ 'className': 'panel-body' },
+            h3({ 'id': 'setHandleModalLabel' }, 'Set your handle'),
+            p({ 'style': { 'display':
+                    'inline' if this.props.handleTaken is True else 'none'
+                }},
+                'Handle already taken try another one'
+                ),
+            form({
+                    'id': 'handleForm',
+                    'onSubmit': this.onHandleSubmit,
+                    'className': 'form-group'
+                    },
+                div({ 'className': 'col-xs-8' },
+                    div({ 'className': 'form-group input-icon' },
+                        i(None, '@'),
+                        input({
+                            'value': this.state.newHandleText,
+                            'onChange': this.updateHandleText,
+                            'type': 'text',
+                            'className': 'form-control',
+                            'id': 'myHandle',
+                            'placeholder': 'handle'
+                            })
+                        )),
+                div({ 'className': 'col-xs-2' },
+                    button({
+                            'id': 'setHandleButton',
+                            'type': 'submit',
+                            'className': 'btn btn-primary'
+                            },
+                        'Set Handle'
+                        )))))
+    })
+
+__pragma__('js', 'export default Settings')
