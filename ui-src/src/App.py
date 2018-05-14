42af4012ff438753b3d62a4ad410708a721dc614
Michael Goldman <mike.goldman@allstreets.org>
2018-05-14 05:29:11 -0400

    Python conversion of first Javascript module

--- ui-src/src/App.py
+++ ui-src/src/App.py
@@ -1,78 +1,90 @@
-import React, { Component } from 'react'
+__pragma__('js', '{}', '''
+import { createElement as e } from 'react'
 import { Route, Link } from 'react-router-dom'
+import createReactClass from 'create-react-class'
 import SettingsContainer from './SettingsContainer'
 import FollowContainer from './FollowContainer'
 import NewMeowContainer from './NewMeowContainer'
 import FollowingFeedContainer from './FollowingFeedContainer'
 import UserFeedContainer from './UserFeedContainer'
 import MeowContainer from './MeowContainer'
+const { div, img, h2, p, a, em, strong } = require('hyperscript-helpers')(e)
+''')
 
-class App extends Component {
-  componentWillMount () {
-    // this fetches the hash which represents the active users userHash
-    this.props.getMyHandle()
-    this.props.getHandles()
-    this.interval = setInterval(this.props.getHandles, 2000)
-  }
+def appComponentWillMount(self):
+    # this fetches the hash which represents the active users userHash
+    self.props.getMyHandle()
+    self.props.getHandles()
+    self.interval = setInterval(self.props.getHandles, 2000)
 
-  componentDidUpdate (prevProps) {
-    // console.log(prevProps.handle, this.props.handle)
-    if (!prevProps.handle && this.props.handle) {
-      this.props.getFollow(this.props.handle, 'following')
-    }
-  }
+def appComponentDidUpdate(self, prevProps):
+    # console.log(prevProps.handle, self.props.handle)
+    if not prevProps.handle and self.props.handle:
+        self.props.getFollow(self.props.handle, 'following')
 
-  componentWillUnmount () {
-    if (this.interval) clearInterval(this.interval)
-  }
+def appComponentWillUnmount(self):
+    if self.interval: clearInterval(self.interval)
 
-  render () {
-    return (
-      <div className='container'>
-        <div className='spinner transition500' />
-        <div className='error transition500' />
-        <div className='row'>
-          <div className='col-sm-2'>
-            <div className='logo'>
-              <img src='/cat-eating-bird-circle.png' alt='cat eating bird' />
-            </div>
-          </div>
-          <div className='col-sm-7'>
-            <div className='contentcontainer'>
-              <div>
-                <Link to='/' id='handle'>{this.props.handle}</Link>
-                <Link to='/settings' id='changeHandleButton' className='btn btn-default'>Settings</Link>
-              </div>
-              <Link to='/follow' id='followButton' className='btn btn-default'>Follow People</Link>
-              <div id='banner'>
-                Clutter
-                <div className='subtitle'>can haz herd cats?</div>
-              </div>
-              <div id='content'>
-                <Route path='/' exact component={NewMeowContainer} />
-                <Route path='/' exact component={FollowingFeedContainer} />
-                <Route path='/u/:handle' component={UserFeedContainer} />
-                <Route path='/settings' component={SettingsContainer} />
-                <Route path='/follow' component={FollowContainer} />
-                <Route path='/meow/:meowHash' component={MeowContainer} />
-              </div>
-            </div>
-          </div>
-          <div className='col-sm-3'>
-            <div className='alphabox'>
-              <div id='about'>
-                <h2>What is Clutter?</h2>
-                <p>A <a href='https://en.wiktionary.org/wiki/clutter'><em>clutter</em></a> is a flock of cats.</p>
-                <p><strong>Clutter</strong> is a fully decentralized alternative to Twitter.</p>
-                <p>Impossible to censor or control.</p>
-                <p>Join the mewvolution on <a href='http://holochain.org'>holochain.org</a>.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+App = createReactClass({
+    'componentWillMount': lambda: appComponentWillMount(this),
 
-export default App
+    'componentDidUpdate': lambda prevProps: appComponentDidUpdate(this, prevProps),
+
+    'componentWillUnmount': lambda: appComponentWillUnmount(this),
+
+    'render': lambda: div({ 'className': 'container' },
+        div({ 'className': 'spinner transition500' }),
+        div({ 'className': 'error transition500' }),
+        div({ 'className': 'row' },
+            div({ 'className': 'col-sm-2' },
+                div({ 'className': 'logo' },
+                    img({ 'src': '/cat-eating-bird-circle.png', 'alt': 'cat eating bird' })
+                    )),
+            div({ 'className': 'col-sm-7' },
+                div({ 'className': 'contentcontainer' },
+                    div(None,
+                        e(Link, { 'to': '/', 'id': 'handle' }, this.props.handle),
+                        e(Link,
+                            { 'to': '/settings', 'id': 'changeHandleButton', 'className': 'btn btn-default' },
+                            'Settings'
+                            )),
+                    e(Link,
+                        { 'to': '/follow', 'id': 'followButton', 'className': 'btn btn-default' },
+                        'Follow People'
+                        ),
+                    div({ 'id': 'banner' },
+                        'Clutter',
+                        div({ 'className': 'subtitle' },
+                            'can haz herd cats?'
+                            )),
+                    div({ 'id': 'content' },
+                        e(Route, { 'path': '/', 'exact': True, 'component': NewMeowContainer }),
+                        e(Route, { 'path': '/', 'exact': True, 'component': FollowingFeedContainer }),
+                        e(Route, { 'path': '/u/:handle', 'component': UserFeedContainer }),
+                        e(Route, { 'path': '/settings', 'component': SettingsContainer }),
+                        e(Route, { 'path': '/follow', 'component': FollowContainer }),
+                        e(Route, { 'path': '/meow/:meowHash', 'component': MeowContainer })
+                        ))),
+            div({ 'className': 'col-sm-3' },
+                div({ 'className': 'alphabox' },
+                    div({ 'id': 'about' },
+                        h2(None, 'What is Clutter?'),
+                        p(None,
+                            'A ',
+                            a({ 'href': 'https://en.wiktionary.org/wiki/clutter' },
+                                em(None, 'clutter')),
+                            ' is a flock of cats.'
+                            ),
+                        p(None,
+                            strong(None, 'Clutter'),
+                            ' is a fully decentralized alternative to Twitter.'
+                            ),
+                        p(None, 'Impossible to censor or control.'),
+                        p(None,
+                            'Join the mewvolution on ',
+                            a({ 'href': 'http://holochain.org' }, 'holochain.org'),
+                            '.'
+                            ))))))
+    })
+
+__pragma__('js', 'export default App')
