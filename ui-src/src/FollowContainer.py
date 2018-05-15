475df0af5440662063e9199231db5c597bf7a81f
Michael Goldman <mike.goldman@allstreets.org>
2018-05-15 05:17:06 -0400

    FollowContainer port is a nice mix of readable syntax

--- ui-src/src/FollowContainer.py
+++ ui-src/src/FollowContainer.py
@@ -1,41 +1,32 @@
+__pragma__('js', '{}', '''
 import { connect } from 'react-redux'
 import Follow from './components/Follow'
-import {
-  follow,
-  unfollow
-} from './actions'
+import { follow, unfollow } from './actions'
+''')
 
-const mapStateToProps = state => {
-  // console.log('state.handles ' + JSON.stringify(state.handles))
-  return {
-    following: Object.keys(state.follows).map(handle => {
-      return {
-        handle
-      }
-    }),
-    notFollowing: Object.keys(state.handles)
-      .filter(handleHash => !state.follows[state.handles[handleHash].handle] && state.handles[handleHash].handle !== state.handle)
-      .map(handleHash => {
-        return {
-          handleHash,
-          handle: state.handles[handleHash].handle
-       }
-      })
-  }
-}
+__pragma__('noalias', 'keys')
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    follow: (handle, then) => {
-      dispatch(follow(handle, then))
-    },
-    unfollow: (handle, then) => {
-      dispatch(unfollow(handle, then))
+def mapStateToProps(state):
+    # console.log('state.handles ' + JSON.stringify(state.handles))
+    return {
+        'following': Object.keys(state.follows).map(
+            lambda handle: { 'handle': handle }),
+
+        'notFollowing': Object.keys(state.handles).filter(
+            lambda handleHash:
+                not state.follows[state.handles[handleHash].handle] and
+                    state.handles[handleHash].handle is not state.handle
+                    ).map(
+            lambda handleHash: {
+                'handleHash': handleHash,
+                'handle': state.handles[handleHash].handle
+                })
+        }
+
+mapDispatchToProps = lambda dispatch: {
+    'follow': lambda handle, then: dispatch(follow(handle, then)),
+    'unfollow': lambda handle, then: dispatch(unfollow(handle, then))
     }
-  }
-}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Follow)
+__pragma__('js',
+    'export default connect(mapStateToProps, mapDispatchToProps)(Follow)')
