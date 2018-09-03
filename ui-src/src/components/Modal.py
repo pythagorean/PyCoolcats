f4e47a42b926d78ad3a9d6f0a67945f1c27e4b8f
Michael Goldman <mike.goldman@allstreets.org>
2018-09-02 21:23:15 -0400

    Converted Modal.py

--- ui-src/src/components/Modal.py
+++ ui-src/src/components/Modal.py
@@ -1,52 +1,49 @@
 __pragma__('js', '{}', '''
-import React from 'react'
+import { createElement as e } from 'react'
+import createReactClass from 'create-react-class'
 import PropTypes from 'prop-types'
+const { div, p } = require('hyperscript-helpers')(e)
+''')
 
-class Modal extends React.Component {
-  render() {
-    // Render nothing if the "show" prop is false
-    if (!this.props.show) {
-      return null
-    }
+def modalRender(self):
+    # Render nothing if the "show" prop is false
+    if not self.props.show:
+        return None
 
-    // The gray background
-    const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'rgba(0,0,0,0.3)',
-      padding: 50
-    }
+    # The gray background
+    backdropStyle = {
+        'position': "fixed",
+        'top': 0,
+        'bottom': 0,
+        'left': 0,
+        'right': 0,
+        'backgroundColor': "rgba(0,0,0,0.3)",
+        'padding': 50
+        }
 
-    // The modal "window"
-    const modalStyle = {
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      maxWidth: 500,
-      minHeight: 200,
-      margin: '0 auto',
-      padding: 30
-    }
+    # the modal "window"
+    modalStyle = {
+        'backgroundColor': '#fff',
+        'borderRadius': 5,
+        'maxWidth': 500,
+        'minHeight': 200,
+        'margin': "0 auto",
+        'padding': 30
+        }
 
-    return (
-      <div style={backdropStyle}>
-        <div style={modalStyle}>
-          <div align="center">
-            <p className="h1">Welcome to Clutter!</p>
-          </div>
-          <div>{this.props.children}</div>
-        </div>
-      </div>
-    )
-  }
-}
+    return div({ 'style': backdropStyle },
+        div({ 'style': modalStyle },
+            div({ 'align': "center" },
+                p({ 'className': "h1" }, "Welcome to Coolcats!")),
+            div(None, self.props.children)))
+
+Modal = createReactClass({
+    'render': lambda: modalRender(this)
+    })
 
 Modal.propTypes = {
-  show: PropTypes.bool,
-  children: PropTypes.node
-}
+    'show': PropTypes.bool,
+    'children': PropTypes.node
+    }
 
-export default Modal
-''')
+__pragma__('js', 'export default Modal')
