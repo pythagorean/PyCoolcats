a39a1d6427a06dc62b894ddabd23bdfb98fce79a
Michael Goldman <mike.goldman@allstreets.org>
2018-09-30 01:18:59 -0400

    First pass UI for image attachments

--- ui-src/src/NewMeow.py
+++ ui-src/src/NewMeow.py
@@ -1,20 +1,49 @@
 __pragma__('js', '{}', '''
 import { createElement as e } from 'react'
 import createReactClass from 'create-react-class'
-const { form, div, textarea, button } = require('hyperscript-helpers')(e)
+const { form, div, textarea, label, input, button } = require('hyperscript-helpers')(e)
 ''')
 
 def newMeowOnSubmit(self, meow):
     meow.preventDefault()
     if not self.state['newMeowText']: return
+    if self.state['newMeowImage']:
+        alert('Has attachment: ' + self.state['newMeowImage'])
+        self.setState({ 'newMeowImage': "" })
     self.props.post(self.state['newMeowText'])
-    self.setState({ 'newMeowText': '' })
+    self.setState({ 'newMeowText': "" })
+
+def newMeowUpdateImage(self, file):
+    input = file.target
+    if input.size > 2000000:
+        alert('File is too big!')
+        return
+    self.upload(input.files[0]).then(
+        lambda dataURL: self.setState({
+            'newMeowImage': dataURL
+            }))
+
+def newMeowUpload(self, file, resolve):
+    reader = __new__(FileReader())
+    reader.onload = lambda read: resolve(read.target.result)
+    reader.readAsDataURL(file)
 
 NewMeow = createReactClass({
-    'getInitialState': lambda: { 'newMeowText': '' },
+    'getInitialState': lambda: {
+        'newMeowText': "",
+        'newMeowImage': ""
+        },
 
-    'updateMeowText': lambda meow: this.setState(
-        { 'newMeowText': meow.target.value }),
+    'updateMeowText': lambda meow: this.setState({
+        'newMeowText': meow.target.value
+        }),
+
+    'updateImage': lambda file: newMeowUpdateImage(this, file),
+
+    'upload':
+        lambda file: __new__(Promise(
+            lambda resolve, reject: newMeowUpload(this, file, resolve)
+            )),
 
     'onMeowSubmit': lambda meow: newMeowOnSubmit(this, meow),
 
@@ -39,6 +68,16 @@ NewMeow = createReactClass({
                 'className': "btn btn-primary"
                 },
             'Meow'
+            ),
+        label(None, "Attach image"),
+        input({
+                'type': "file",
+                'accept': "image/*",
+                'ref': this.state.newMeowImage,
+                'onChange': this.updateImage,
+                'hidden': True,
+                'id': "image",
+                }
             ))
     })
 
